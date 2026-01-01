2eb21a920180aeff4464b5473d8181c5fd54fc97
Lionel Schiepers <lionel_schiepers@hotmail.com>
2026-01-01 19:32:40 +0100

    new script to rebuild the solution with cleaning

---
+++ rebuild.cmd
@@ -0,0 +1,9 @@
+rd /s /q node_modules
+rd /s /q dist
+del package-lock.json
+cmd /c npm.cmd i
+cmd /c npm.cmd outdated
+cmd /c npm.cmd run lint
+cmd /c npm.cmd run format
+cmd /c npm.cmd run build
+
